1a855723aed731b56ab084c08bc0ba8ffd6dfdc5
Bazigar ( CFC ) <hdhindihub@gmail.com>
2025-08-28 18:28:10 +0530

    Update stats.py

--- bot/stats.py
+++ bot/stats.py
@@ -1,1 +1,9 @@
+from telegram import Update
+from telegram.ext import CallbackContext
+from utils.db import get_user_stats
 
+def handle_unostats(update: Update, ctx: CallbackContext):
+    user_id = update.effective_user.id
+    stats = get_user_stats(user_id)
+    ctx.bot.send_message(
+        update.e
