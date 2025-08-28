fff1935f456c764b3fb41564251a8a069c89a9c3
Bazigar ( CFC ) <hdhindihub@gmail.com>
2025-08-28 18:45:10 +0530

    Update stickers.py

--- bot/stickers.py
+++ bot/stickers.py
@@ -1,7 +1,71 @@
-# Map internal card codes (like "red_5") to the Telegram sticker file IDs
+# utils/stickers.py
+
+# Mapping UNO card names to Telegram sticker file_ids.
+# IMPORTANT: Replace the placeholder strings with actual file_id
+# values retrieved from your bot by sending the UNO stickers.
+
 CARD_STICKER_MAP = {
-    "red_0": "CAACAgUAAxkBAAEE1Xp...",  # Example file_id
-    "red_1": "CAACAgUAAxkBAAEE1Xq...",
-    # ... include all UNO cards ...
-    "wild_draw_four": "CAACAgUAAxkBAAEE2Aa..."
+    # Red cards
+    "red_0": "file_id_red_0",
+    "red_1": "file_id_red_1",
+    "red_2": "file_id_red_2",
+    "red_3": "file_id_red_3",
+    "red_4": "file_id_red_4",
+    "red_5": "file_id_red_5",
+    "red_6": "file_id_red_6",
+    "red_7": "file_id_red_7",
+    "red_8": "file_id_red_8",
+    "red_9": "file_id_red_9",
+    "red_skip": "file_id_red_skip",
+    "red_reverse": "file_id_red_reverse",
+    "red_draw_two": "file_id_red_draw_two",
+
+    # Green cards
+    "green_0": "file_id_green_0",
+    "green_1": "file_id_green_1",
+    "green_2": "file_id_green_2",
+    "green_3": "file_id_green_3",
+    "green_4": "file_id_green_4",
+    "green_5": "file_id_green_5",
+    "green_6": "file_id_green_6",
+    "green_7": "file_id_green_7",
+    "green_8": "file_id_green_8",
+    "green_9": "file_id_green_9",
+    "green_skip": "file_id_green_skip",
+    "green_reverse": "file_id_green_reverse",
+    "green_draw_two": "file_id_green_draw_two",
+
+    # Blue cards
+    "blue_0": "file_id_blue_0",
+    "blue_1": "file_id_blue_1",
+    "blue_2": "file_id_blue_2",
+    "blue_3": "file_id_blue_3",
+    "blue_4": "file_id_blue_4",
+    "blue_5": "file_id_blue_5",
+    "blue_6": "file_id_blue_6",
+    "blue_7": "file_id_blue_7",
+    "blue_8": "file_id_blue_8",
+    "blue_9": "file_id_blue_9",
+    "blue_skip": "file_id_blue_skip",
+    "blue_reverse": "file_id_blue_reverse",
+    "blue_draw_two": "file_id_blue_draw_two",
+
+    # Yellow cards
+    "yellow_0": "file_id_yellow_0",
+    "yellow_1": "file_id_yellow_1",
+    "yellow_2": "file_id_yellow_2",
+    "yellow_3": "file_id_yellow_3",
+    "yellow_4": "file_id_yellow_4",
+    "yellow_5": "file_id_yellow_5",
+    "yellow_6": "file_id_yellow_6",
+    "yellow_7": "file_id_yellow_7",
+    "yellow_8": "file_id_yellow_8",
+    "yellow_9": "file_id_yellow_9",
+    "yellow_skip": "file_id_yellow_skip",
+    "yellow_reverse": "file_id_yellow_reverse",
+    "yellow_draw_two": "file_id_yellow_draw_two",
+
+    # Wild cards
+    "wild": "file_id_wild",
+    "wild_draw_four": "file_id_wild_draw_four",
 }
